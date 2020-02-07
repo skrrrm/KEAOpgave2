be7d4fc0db08607d425b5065a26077dd84c5e6bf
skrrrm <a@macbook-windows>
2020-02-07 10:08:46 +0100

    fixes and comments

--- KEAOpgave2/Form1.cs
+++ KEAOpgave2/Form1.cs
@@ -17,11 +17,14 @@ namespace KEAOpgave2
             InitializeComponent();
         }
 
+        // Variabler
+        double a, b, res;
         private void buttonSum_Click(object sender, EventArgs e) // Crasher hvis der ikke indskrives tal
         {
-            double a = Convert.ToDouble(textBox1.Text); // Konvertere textbox1 til en double, så der kan regnes på det, da det jo er en string
-            double b = Convert.ToDouble(textBox2.Text); // --------- || ---------
-            labelResult.Text = Convert.ToString(a+b); // Lægger a og b sammen og konvertere til tekst
+            a = Convert.ToDouble(textBox1.Text); // Konvertere textbox1 til en double, så der kan regnes på det, da det jo er en string
+            b = Convert.ToDouble(textBox2.Text); // --------- || ---------
+            res = a + b; // Lægger a og b sammen og konvertere til tekst
+            labelResult.Text = Convert.ToString(res); // Konvertere 'res' til en string så den kan smækkes ind i 'labelResult'
         }
     }
 }
