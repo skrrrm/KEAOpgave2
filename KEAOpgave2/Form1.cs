16bd91c7853877eea5cee335c6df1ad1945888e0
skrrrm <a@macbook-windows>
2020-02-07 10:15:48 +0100

    TryCatch

--- KEAOpgave2/Form1.cs
+++ KEAOpgave2/Form1.cs
@@ -21,10 +21,19 @@ namespace KEAOpgave2
         double a, b, res;
         private void buttonSum_Click(object sender, EventArgs e) // Crasher hvis der ikke indskrives tal
         {
-            a = Convert.ToDouble(textBox1.Text); // Konvertere textbox1 til en double, så der kan regnes på det, da det jo er en string
-            b = Convert.ToDouble(textBox2.Text); // --------- || ---------
+            try
+            {
+                a = Convert.ToDouble(textBox1.Text); // Konvertere textbox1 til en double, så der kan regnes på det, da det jo er en string
+                b = Convert.ToDouble(textBox2.Text); // --------- || ---------
+            }
+            catch
+            {
+                labelResult.Text = "Indtast kun tal";
+            }
+
             res = a + b; // Lægger a og b sammen og konvertere til tekst
             labelResult.Text = Convert.ToString(res); // Konvertere 'res' til en string så den kan smækkes ind i 'labelResult'
+
         }
     }
 }
